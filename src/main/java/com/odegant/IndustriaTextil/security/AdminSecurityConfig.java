d6ffeba180bc2f7bef06efca16e1e3a4db639fb3
tavodgant <ta_o1993@hotmail.com>
2021-06-05 12:12:17 -0500

    Update Fragments and Security URL

--- src/main/java/com/odegant/IndustriaTextil/security/AdminSecurityConfig.java
+++ src/main/java/com/odegant/IndustriaTextil/security/AdminSecurityConfig.java
@@ -28,7 +28,7 @@ public class AdminSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable().authorizeRequests().antMatchers("/").permitAll()
-                .antMatchers("/admin").hasRole("ADMIN")
+                .antMatchers("/admin/**").hasRole("ADMIN")
                 .and().formLogin().loginPage("/login")
                 .defaultSuccessUrl("/admin", true);
     }
