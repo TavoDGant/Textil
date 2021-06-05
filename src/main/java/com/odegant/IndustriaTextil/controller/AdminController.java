5a1484db3928527b699dca40f499a4f71163781a
tavodgant <ta_o1993@hotmail.com>
2021-06-04 23:32:08 -0500

    Update Form ValidationMessages

--- src/main/java/com/odegant/IndustriaTextil/controller/AdminController.java
+++ src/main/java/com/odegant/IndustriaTextil/controller/AdminController.java
@@ -8,8 +8,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.ArrayList;
 import java.util.Objects;
 
@@ -63,9 +65,14 @@ public class AdminController {
         return "nuevo-empleado";
     }
 
+    //@ModelAttribute("empleado")
     @PostMapping("/agregar")
-    public String agregarEmpleado(@ModelAttribute("empleado") Empleado empleado){
-        adminService.guardarEmpleado(empleado);
-        return "redirect:/admin";
+    public String agregarEmpleado(@Valid Empleado empleado, BindingResult result){
+        if(result.hasErrors()){
+            return "nuevo-empleado";
+        }else{
+            adminService.guardarEmpleado(empleado);
+            return "redirect:/admin";
+        }
     }
 }
--- src/main/java/com/odegant/IndustriaTextil/entity/Empleado.java
+++ src/main/java/com/odegant/IndustriaTextil/entity/Empleado.java
@@ -4,6 +4,8 @@ import lombok.Data;
 import org.hibernate.annotations.DynamicUpdate;
 import org.springframework.format.annotation.DateTimeFormat;
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.util.Calendar;
 import java.util.List;
 
@@ -19,18 +21,22 @@ public class Empleado {
     private Integer id_empleado;
 
     @Column(name = "nombre", nullable = false, length = 30)
+    @NotBlank(message = "{tamanioNombre.Empleado.nombre}")
     private String nombre;
 
     @Column(name = "ape_pat", nullable = false, length = 30)
+    @NotBlank(message = "{tamanioApePat.Empleado.ape_pat}")
     private String ape_pat;
 
     @Column(name = "ape_mat", nullable = false, length = 30)
+    @NotBlank(message = "{tamanioApeMat.Empleado.ape_mat}")
     private String ape_mat;
 
     @Column(name = "registro", nullable = false)
     @DateTimeFormat(pattern = "yyyy-MM-dd")
     private Calendar registro;
 
+    @NotNull(message = "{tamanioNacimiento.Empleado.nacimiento}")
     @Column(name = "nacimiento", nullable = false)
     @DateTimeFormat(pattern = "yyyy-MM-dd")
     private Calendar nacimiento;
